96ff05877a1cf2447202a56ea2f1d9655ba0be6f
SCUT201710098311/SCUT201710098311 <1459835771@qq.com>
2020-07-31 16:56:31 +0800

    关注功能

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -1,10 +1,16 @@
 package com.nowcoder.community.util;
 
+import java.util.List;
+import java.util.Map;
+
 public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE="like:entity";
     private static final String PREFIX_USER_LIKE="like:user";
     private static final String PREFIX_USER="user";
+    //关注功能需要记录两份数据，方便统计
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
 
 
     //某个实体的赞
@@ -22,4 +28,21 @@ public class RedisKeyUtil {
     public static String getUser(int userId){
         return PREFIX_USER+SPLIT+userId;
     }
+
+   //某个用户关注的实体
+    //followee : userId:entityType --> zset(entityId,now)
+    public static  String getFolloweeKey(int userId,int entityType){
+        return PREFIX_FOLLOWEE + SPLIT +userId + SPLIT +entityType;
+
+    }
+
+    //某个实体拥有的粉丝
+    //follower :entityType:entityId --> zset(userId,now）
+
+    public static  String getFollowerKey(int entityType,int entityId){
+        return PREFIX_FOLLOWER + SPLIT +entityType + SPLIT +entityId;
+
+    }
+
+
 }
